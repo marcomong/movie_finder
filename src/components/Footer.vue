bfb772bdb442820bcb8e776cb8ca5a763a3fdf5e
marco <mongelli.marco.8@gmail.com>
2019-10-31 23:02:07 -0500

    adds the movie db logo

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="footer">
-      Made with hope
+      <div class="footer__half">
+        Made with ??? and coffee
+      </div>
+      <div class="footer__half">
+        <img src="https://www.themoviedb.org/assets/2/v4/logos/powered-by-rectangle-green-dcada16968ed648d5eb3b36bbcfdd8cdf804f723dcca775c8f2bf4cea025aad6.svg" alt="powered by movied db" class="footer__image">
+      </div>
     </div>
 </template>
 
